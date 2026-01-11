5eeda5f3cf1cfe3e14fbf10b8a86505a543cf316
denzel <denzelwashingtonsantana@gmail.com>
2026-01-11 20:04:11 -0300

    Add route and logic to login and logout

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 
+load_dotenv()
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev-insecure-change-me")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ecommerce.db"
 
+login_manager = LoginManager()
 db = SQLAlchemy(app)
+login_manager.init_app(app)
+login_manager.login_view = "login"
 CORS(app)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
 
 
 class Product(db.Model):
@@ -16,7 +32,31 @@ class Product(db.Model):
     description = db.Column(db.Text, nullable=True)
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    user = User.query.filter_by(username=data.get("username")).first()
+
+    if user and data.get("password") == user.password:
+        login_user(user)
+        return jsonify({"message": "User logged in successfully"})
+    return jsonify({"message": "Unauthorized. Invalid credentials"}), 401
+
+
+@app.route("/logout", methods=["POST"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout successfully"})
+
+
 @app.route("/api/products/add", methods=["POST"])
+@login_required
 def add_product():
     data = request.json
 
@@ -52,6 +92,7 @@ def add_product():
 
 
 @app.route("/api/products/delete/<int:product_id>", methods=["DELETE"])
+@login_required
 def delete_product(product_id):
     product = Product.query.get(product_id)
     if product:
@@ -77,6 +118,7 @@ def get_product_details(product_id):
 
 
 @app.route("/api/products/update/<int:product_id>", methods=["PUT"])
+@login_required
 def update_product(product_id):
     product = Product.query.get(product_id)
     if not product:
